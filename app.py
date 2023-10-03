122fbef6e4d6427e3b10f86917dd43a68a7dfa92
JollyDataScientist <110872368+JollyDataScientist@users.noreply.github.com>
2023-10-02 21:08:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -12,28 +12,28 @@ def main():
     st.header("Basics")
     basics_expenses = 0
 
-    with st.beta_expander("Rent/Mortgage"):
-        col1, col2 = st.beta_columns(2)
+    with st.expander("Rent/Mortgage"):
+        col1, col2 = st.columns(2)
         rent_mortgage = col2.number_input("Rent/Mortgage:", value=0.00, step=0.01)
         basics_expenses += rent_mortgage
 
-    with st.beta_expander("Food"):
-        col1, col2 = st.beta_columns(2)
+    with st.expander("Food"):
+        col1, col2 = st.columns(2)
         groceries = col2.number_input("Groceries:", value=0.00, step=0.01)
-        col3, col4 = st.beta_columns(2)
+        col3, col4 = st.columns(2)
         eating_out = col4.number_input("Eating Out:", value=0.00, step=0.01)
         basics_expenses += groceries + eating_out
 
-    with st.beta_expander("Essentials"):
-        col1, col2 = st.beta_columns(2)
+    with st.expander("Essentials"):
+        col1, col2 = st.columns(2)
         power = col2.number_input("Power:", value=0.00, step=0.01)
-        col3, col4 = st.beta_columns(2)
+        col3, col4 = st.columns(2)
         water = col4.number_input("Water:", value=0.00, step=0.01)
-        col5, col6 = st.beta_columns(2)
+        col5, col6 = st.columns(2)
         heat = col6.number_input("Heat:", value=0.00, step=0.01)
-        col7, col8 = st.beta_columns(2)
+        col7, col8 = st.columns(2)
         toiletries = col8.number_input("Toiletries:", value=0.00, step=0.01)
-        col9, col10 = st.beta_columns(2)
+        col9, col10 = st.columns(2)
         other_consumables = col10.number_input("Other Consumables:", value=0.00, step=0.01)
         basics_expenses += power + water + heat + toiletries + other_consumables
 
