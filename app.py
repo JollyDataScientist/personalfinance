f8486fbf7d5f7e389645a8169f37a6c41d0c2b15
JollyDataScientist <110872368+JollyDataScientist@users.noreply.github.com>
2023-10-02 20:57:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,51 @@
-# Import necessary libraries
 import streamlit as st
 
-# Streamlit app
 def main():
-    st.title("Personal Income Spending Flowchart")
+    st.title("Monthly Expenses Tracker")
     
-    # Phase 1
-    st.header("Phase 1")
-    create_budget = st.selectbox("Have you created a budget?", ["Select", "Yes", "No"])
-    if create_budget == "No":
-        st.write("Step 1: Establish a budget and set realistic goals.")
-        return
+    # Sidebar
+    st.sidebar.title("Summary")
+    net_monthly = st.sidebar.number_input("Net Monthly Take-Home:", value=0.00, step=0.01)
+    total_expenses = 0
 
-    # Step 1
-    emergency_fund = st.selectbox("Have you built an emergency fund?", ["Select", "Yes", "No"])
-    if emergency_fund == "No":
-        st.write("Step 2: Build an emergency fund.")
-        return
+    # Basics Section
+    st.header("Basics")
 
-    # Step 2
-    employer_match = st.selectbox("Does your employer offer a retirement account with an employer match?", ["Select", "Yes", "No"])
-    if employer_match == "Yes":
-        st.write("Contribute the amount needed to get the full employer match, but nothing above that amount.")
-    elif employer_match == "No":
-        # Continue the flow with the next questions as per the flowchart...
-        pass
+    rent_mortgage = st.number_input("Rent/Mortgage:", value=0.00, step=0.01)
+    total_expenses += rent_mortgage
 
-    # Continue with more questions and steps based on the flowchart...
-    
-    # ... add code here ...
+    st.subheader("Food")
+    groceries = st.number_input("Groceries:", value=0.00, step=0.01)
+    eating_out = st.number_input("Eating Out:", value=0.00, step=0.01)
+    total_expenses += groceries + eating_out
 
-    # Finally
-    st.write("Here are your recommended steps based on your inputs: ...")
-    
-# Run the app
+    st.subheader("Essentials")
+    power = st.number_input("Power:", value=0.00, step=0.01)
+    water = st.number_input("Water:", value=0.00, step=0.01)
+    heat = st.number_input("Heat:", value=0.00, step=0.01)
+    toiletries = st.number_input("Toiletries:", value=0.00, step=0.01)
+    other_consumables = st.number_input("Other Consumables:", value=0.00, step=0.01)
+    total_expenses += power + water + heat + toiletries + other_consumables
+
+    st.subheader("Monthly Expenses")
+    car_payments = st.number_input("Car Payments:", value=0.00, step=0.01)
+    cell_phone = st.number_input("Cell Phone:", value=0.00, step=0.01)
+    internet = st.number_input("Internet:", value=0.00, step=0.01)
+    media_streaming = st.number_input("Media/Streaming:", value=0.00, step=0.01)
+    other_subscriptions = st.number_input("Other Subscriptions:", value=0.00, step=0.01)
+    total_expenses += car_payments + cell_phone + internet + media_streaming + other_subscriptions
+
+    st.subheader("Outstanding Debts")
+    student_loan = st.number_input("Student Loan:", value=0.00, step=0.01)
+    credit_card = st.number_input("Credit Card:", value=0.00, step=0.01)
+    line_of_credit = st.number_input("Line of Credit:", value=0.00, step=0.01)
+    other_loans = st.number_input("Other Loans:", value=0.00, step=0.01)
+    total_expenses += student_loan + credit_card + line_of_credit + other_loans
+
+    # Display Summary on Sidebar
+    st.sidebar.text(f"Total Expenses: ${total_expenses:,.2f}")
+    remaining_balance = net_monthly - total_expenses
+    st.sidebar.text(f"Remaining Balance: ${remaining_balance:,.2f}")
+
 if __name__ == "__main__":
     main()
